e4e7520a74986d4e872e1ef9b3ea168b12517271
happyhermann <carrick035@gmail.com>
2022-09-05 12:46:39 +0900

    Complete CSS styles and comments function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 // 바뀔 가능성이 있는 변수는 뺴주는게 좋다 URL
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -15,36 +18,69 @@ function getData(url) {
 //응답 값을 객체로 바꿔보자 (preview 탭)
 // JSON 데이터를 객체로 바꾸는 도구는 'json.parse)이다
 
-const ul = document.createElement("ul");
-
 // 객체화되어 있을 것 (axios에서는 이 기능을 자동으로 처리해줌)
 
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
-
-  newsList.push("<ul>");
-
-  for (let i = 0; i < 10; i++) {
-    `
+  let template = `
+  <div class="bg-gray-600 min-h-screen">
+  <div class="bg-white text-xl">
+    <div class="mx-auto px-4">
+      <div class="flex justify-between items-center py-6">
+        <div class="flex justify-start">
+          <h1 class="font-extrabold">Hacker News</h1>
+        </div>
+        <div class="items-center justify-end">
+          <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+            Previous
+          </a>
+          <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+            Next
+          </a>
+        </div>
+      </div> 
+    </div>
+  </div>
+  <div class="p-4 text-2xl text-gray-700">
+    {{__news_feed__}}        
+  </div>
+</div>
   `;
 
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-      <li>
-        <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count}) (comments)
-        </a>
-      </li>
- `);
-
-    // ** 문자열만을 가지고 UI를 만드는 것, DOM API를 최대한 이용하지 않는 것이
-    // 실제 프로젝트가 커지고 코드가 복잡해질 수록 권장된다.
-    // 마크업 구조가 잘 보인다는 것이다. 문자열을 사용하면 좋은 점
+    <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+          </div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>    
+    `);
   }
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace(
+    "{{__prev_page__}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace("{{__next_page__}}", store.currentPage + 1);
 
-  newsList.push(`</ul>`);
+  container.innerHTML = template;
 
-  container.innerHTML = newsList.join("");
+  // ** 문자열만을 가지고 UI를 만드는 것, DOM  API를 최대한 이용하지 않는 것이
+  // 실제 프로젝트가 커지고 코드가 복잡해질 수록 권장된다.
+  // 마크업 구조가 잘 보인다는 것이다. 문자열을 사용하면 좋은 점
 
   // join : 배열 요소 안에 문자열들을 하나의 문자열로 합쳐서 반환 (기본 , 구분자가 있음)
   // join('') 빈 문자열을 넣어주면 구분자 없이 하나의 합쳐진 HTML 문자열을 얻을 수 있음
@@ -53,18 +89,45 @@ function newsFeed() {
 }
 
 function newsDetail() {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
 
   // 쓰고 싶은 위치값부터 그 이후부터만 나오게
   // 1은 "#"을 제외한 나머지의 문자열만 나오게 되는 것이다.
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div>
+  `;
 
   container.innerHTML = `  
       <h1>${newsContent.title}</h1>
 
       <div>
-        <a href="#">목록으로</a>
+        <a href="#/page/${store.currentPage}">목록으로</a>
       </div>
     
     `;
@@ -74,11 +137,43 @@ function newsDetail() {
   // content.appendChild(title);
 
   // 문자열 함수 중에 값을 대체해주는 replace 함수가 있다
+
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+
+    for (let i = 0; i < comments.length; i++) {
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>      
+      `);
+
+      if (comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1));
+      }
+    }
+
+    return commentString.join("");
+  }
+
+  container.innerHTML = template.replace(
+    "{{__comments__}}",
+    makeComment(newsContent.comments)
+  );
 }
 
 function router() {
   const routePath = location.hash;
+  console.log(routePath);
+
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
